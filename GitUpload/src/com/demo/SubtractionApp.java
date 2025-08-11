590fca4a74fd4c369c7fbe6200aa1f159a6368d1
Gulzar <gulzarshikilkar123@gmail.com>
2025-08-11 17:39:05 +0530

    Added SubtractionApp

---
+++ GitUpload/src/com/demo/SubtractionApp.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class SubtractionApp {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+       System.out.println("Enter two No");
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       System.out.println("Subtraction Is ="+(a-b));
+	}
+}
