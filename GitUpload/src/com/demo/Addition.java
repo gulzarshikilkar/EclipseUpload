e27ac43e4117d445abff2c3a9d40a25d6203f5a5
Gulzar <gulzarshikilkar123@gmail.com>
2025-08-11 17:06:10 +0530

    This is Eclipse java Project

---
+++ GitUpload/src/com/demo/Addition.java
@@ -0,0 +1,14 @@
+package com.demo;
+import java.util.*;
+public class Addition {
+      
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+       System.out.println("Enter two No");
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       System.out.println("Addition Is ="+(a+b));
+	}
+
+}
